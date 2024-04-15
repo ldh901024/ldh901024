c7bde22f123dcbd74b876500437f912084021eb4
ldh901024 <ldh901024@naver.com>
2024-04-15 11:44:18 +0900

    Web_Dev

--- Web/app.py
+++ Web/app.py
@@ -7,14 +7,15 @@ from flask import Flask, session
 app = Flask(__name__)
 app.secret_key = 'Dlcmdls23!@'
 
-
-#logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+# logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s - %(levelname)s - %(message)s',
     filename='./logfile.log',  # 로그 파일 경로와 이름
     filemode='a'  # 'a'는 로그 파일에 내용을 추가하는 모드입니다. 'w'로 설정하면 파일을 쓸 때마다 내용이 덮어씌워집니다.
 )
+
+
 # 로컬 데이터베이스 연결 설정 함수
 def get_local_db_connection():
     try:
@@ -27,6 +28,7 @@ def get_local_db_connection():
     except Exception as e:
         logging.error("Local database connection failed: %s", e)
         raise
+
 
 # 외부 데이터베이스 연결 설정 함수
 def get_external_db_connection():
@@ -41,7 +43,6 @@ def get_external_db_connection():
     except Exception as e:
         logging.error("External database connection failed: %s", e)
         raise
-
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -80,7 +81,6 @@ def logout():
     return redirect(url_for('login'))
 
 
-
 @app.route('/')
 def home():
     try:
@@ -92,7 +92,8 @@ def home():
 
         # 외부 데이터베이스에서 장치 정보를 조회
         external_cursor = external_connection.cursor(dictionary=True)
-        external_cursor.execute("SELECT d.device_id, c.customer_nm, d.host_nm, d.serial_num FROM tb_device d JOIN tb_customer c ON d.customer_id = c.customer_id")
+        external_cursor.execute(
+            "SELECT d.device_id, c.customer_nm, d.host_nm, d.serial_num FROM tb_device d JOIN tb_customer c ON d.customer_id = c.customer_id")
         devices = external_cursor.fetchall()
         external_cursor.close()
         external_connection.close()
@@ -125,17 +126,16 @@ def test_page():
     try:
         if 'username' not in session:  # 사용자가 로그인하지 않았다면
             return redirect(url_for('login'))  # 로그인 페이지로 리디렉션
-    
+
     except Exception as e:
         logging.error("Error fetching data: %s", e)
         devices = []  # 오류 발생 시 빈 리스트 반환
-    
+
     return render_template('test.html')
 
 
 @app.route('/security_rules')
 def security_rules():
-
     if 'username' not in session:  # 사용자가 로그인하지 않았다면
         return redirect(url_for('login'))  # 로그인 페이지로 리디렉션
 
@@ -144,9 +144,8 @@ def security_rules():
         conn = get_local_db_connection()
         cursor = conn.cursor(dictionary=True)
 
-
         # SecurityRules 테이블에서 모든 보안 규칙을 조회
-        cursor.execute("SELECT id, rule, description FROM SecurityRules")
+        cursor.execute("SELECT id, rule, description, tactics, technique  FROM SecurityRules")
         rules = cursor.fetchall()
 
         cursor.close()
@@ -168,7 +167,9 @@ def submit():
         for external_device_id in request.form.getlist('external_device_id'):
             # 외부 DB에서 해당 device_id의 정보를 조회
             external_cursor = external_connection.cursor(dictionary=True)
-            external_cursor.execute("SELECT d.device_id, c.customer_nm, d.host_nm, d.serial_num FROM tb_device d JOIN tb_customer c ON d.customer_id = c.customer_id WHERE d.device_id = %s", (external_device_id,))
+            external_cursor.execute(
+                "SELECT d.device_id, c.customer_nm, d.host_nm, d.serial_num FROM tb_device d JOIN tb_customer c ON d.customer_id = c.customer_id WHERE d.device_id = %s",
+                (external_device_id,))
             device_info = external_cursor.fetchone()
             external_cursor.close()
 
@@ -182,7 +183,9 @@ def submit():
                 insert_query = '''INSERT INTO tb_device_checks (device_id, customer_name, host_name, serial_num, sms_check, email_check)
                   VALUES (%s, %s, %s, %s, %s, %s)
                   ON DUPLICATE KEY UPDATE customer_name = VALUES(customer_name), host_name = VALUES(host_name), serial_num = VALUES(serial_num), sms_check = VALUES(sms_check), email_check = VALUES(email_check)'''
-                local_cursor.execute(insert_query, (external_device_id, device_info['customer_nm'], device_info['host_nm'], device_info['serial_num'], sms_check, email_check))
+                local_cursor.execute(insert_query, (
+                external_device_id, device_info['customer_nm'], device_info['host_nm'], device_info['serial_num'],
+                sms_check, email_check))
                 local_connection.commit()
                 local_cursor.close()
 
@@ -193,6 +196,7 @@ def submit():
 
     return redirect(url_for('home'))
 
+
 @app.route('/add_rule', methods=['POST'])
 def add_rule():
     if 'username' not in session:
@@ -200,12 +204,14 @@ def add_rule():
 
     rule_name = request.form['rule_name']
     description = request.form['description']
+    tactics = request.form['tactics']
+    technique = request.form['technique']
 
     try:
         conn = get_local_db_connection()
         cursor = conn.cursor()
 
-        cursor.execute("INSERT INTO SecurityRules (rule, description) VALUES (%s, %s)", (rule_name, description))
+        cursor.execute("INSERT INTO SecurityRules (rule, description, tactics, technique) VALUES (%s, %s, %s, %s)", (rule_name, description, tactics, technique))
         conn.commit()
 
         cursor.close()
@@ -216,6 +222,7 @@ def add_rule():
 
     return redirect(url_for('security_rules'))
 
+
 @app.route('/edit_rule/<int:rule_id>')
 def edit_rule(rule_id):
     if 'username' not in session:
@@ -224,13 +231,14 @@ def edit_rule(rule_id):
     conn = get_local_db_connection()
     cursor = conn.cursor(dictionary=True)
 
-    cursor.execute("SELECT id, rule, description FROM SecurityRules WHERE id = %s", (rule_id,))
+    cursor.execute("SELECT id, rule, description, tactics, technique FROM SecurityRules WHERE id = %s", (rule_id,))
     rule = cursor.fetchone()
 
     cursor.close()
     conn.close()
 
     return render_template('edit_rule.html', rule=rule)
+
 
 @app.route('/update_rule/<int:rule_id>', methods=['POST'])
 def update_rule(rule_id):
@@ -239,17 +247,21 @@ def update_rule(rule_id):
 
     rule_name = request.form['rule_name']
     description = request.form['description']
+    tactics = request.form['tactics']
+    technique = request.form['technique']
 
     conn = get_local_db_connection()
     cursor = conn.cursor()
 
-    cursor.execute("UPDATE SecurityRules SET rule = %s, description = %s WHERE id = %s", (rule_name, description, rule_id))
+    cursor.execute("UPDATE SecurityRules SET rule = %s, description = %s WHERE id = %s",
+                   (rule_name, description, rule_id, tactics, technique))
     conn.commit()
 
     cursor.close()
     conn.close()
 
     return redirect(url_for('security_rules'))
+
 
 @app.route('/delete_rule/<int:rule_id>')
 def delete_rule(rule_id):
